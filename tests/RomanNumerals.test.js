28d21ae03c2f3c49d6a6eee5d657bf67ede6a10c
Rafael Minguet <rminguet@gmail.com>
2017-11-01 17:12:30 +0100

    Unit conversion from 1 to 899999

--- tests/RomanNumerals.test.js
+++ tests/RomanNumerals.test.js
@@ -30,13 +30,45 @@ const RomanNumerals = function () {
         700: 'DCC',
         800: 'DCCC',
         900: 'CM',
-        1000: 'M'
+        1000: 'M',
+        2000: 'MM',
+        3000: 'MMM'
     };
 
     const toRoman = function (number) {
         const stringNumber = number.toString();
-        const arrayNumber = stringNumber.split('');
-        return arrayNumber.reverse().reduce((acc, val, index) => {
+        const splitNumber = getThousands(stringNumber.split(''));
+        let inRoman = '';
+
+        if (number > 3999) {
+            // We can write letters with overline... so instead of an overline we add parenthesis for thousands
+            inRoman = `(${romanRepresentation(splitNumber.thousands)})${romanRepresentation(splitNumber.units)}`;
+        } else {
+            inRoman = romanRepresentation(splitNumber.thousands.concat(splitNumber.units));
+        }
+
+        return inRoman;
+    };
+
+    /**
+     *
+     * @param   splitNumber     Number with its units in an array: 3498 = [3, 4, 9, 8]
+     * @returns {*}             Object like this: 3498 => return {thousands: [3], units: [4, 9, 8]}
+     */
+    const getThousands = function (splitNumber) {
+        return splitNumber.reverse().reduce((acc, val, index) => {
+            if (index < 3) {
+                acc.units.unshift(val);
+            } else {
+                acc.thousands.unshift(val);
+            }
+
+            return acc;
+        }, {thousands: [], units: []});
+    };
+
+    const romanRepresentation = function (splitNumber) {
+        return splitNumber.reverse().reduce((acc, val, index) => {
             const unit = val * (Math.pow(10, index));
             const numberInRoman = NUMBERS_CONVERSION[unit];
             acc = numberInRoman + acc;
@@ -83,7 +115,7 @@ test('-------- Centurion: unit conversion between numeral systems [1-100]', (ass
     assert.end();
 });
 
-test('-------- Centurion: unit conversion between numeral systems [1-1000]', (assert) => {
+test('-------- Praetorian prefect: unit conversion between numeral systems [1-1000]', (assert) => {
     const messageForNumber467Conversion = `'CDLXVII' stands for '467' in roman numerals`;
     const messageForNumber849Conversion = `'DCCCXLIX' stands for '849' in roman numerals`;
     const expectedRomanNumeral467 = 'CDLXVII';
@@ -96,6 +128,48 @@ test('-------- Centurion: unit conversion between numeral systems [1-1000]', (as
 
     assert.equal(actualRomanNumeral467, expectedRomanNumeral467, messageForNumber467Conversion);
     assert.equal(actualRomanNumeral849, expectedRomanNumeral849, messageForNumber849Conversion);
+
+    assert.end();
+});
+
+test('-------- Praetorian prefect: unit conversion between numeral systems [1-10000]', (assert) => {
+    const messageForNumber3467Conversion = `'MMMCDLXVII' stands for '3467' in roman numerals`;
+    const messageForNumber4467Conversion = `'(IV)CDLXVII' stands for '4467' in roman numerals`;
+    const messageForNumber7849Conversion = `'(VII)DCCCXLIX' stands for '7849' in roman numerals`;
+    const expectedRomanNumeral3467 = 'MMMCDLXVII';
+    const expectedRomanNumeral4467 = '(IV)CDLXVII';
+    const expectedRomanNumeral7849 = '(VII)DCCCXLIX';
+
+    const romanNumerals = RomanNumerals();
+
+    const actualRomanNumeral3467 = romanNumerals.toRoman(3467);
+    const actualRomanNumeral4467 = romanNumerals.toRoman(4467);
+    const actualRomanNumeral7849 = romanNumerals.toRoman(7849);
+
+    assert.equal(actualRomanNumeral3467, expectedRomanNumeral3467, messageForNumber3467Conversion);
+    assert.equal(actualRomanNumeral4467, expectedRomanNumeral4467, messageForNumber4467Conversion);
+    assert.equal(actualRomanNumeral7849, expectedRomanNumeral7849, messageForNumber7849Conversion);
+
+    assert.end();
+});
+
+test('-------- Centurion: unit conversion between numeral systems [1-899999]', (assert) => {
+    const messageForNumber33467Conversion = `'(XXXIII)CDLXVII' stands for '33467' in roman numerals`;
+    const messageForNumber444467Conversion = `'(CDXLIV)CDLXVII' stands for '444467' in roman numerals`;
+    const messageForNumber869849Conversion = `'(DCCCLXIX)DCCCXLIX' stands for '869849' in roman numerals`;
+    const expectedRomanNumeral33467 = '(XXXIII)CDLXVII';
+    const expectedRomanNumeral444467 = '(CDXLIV)CDLXVII';
+    const expectedRomanNumeral869849 = '(DCCCLXIX)DCCCXLIX';
+
+    const romanNumerals = RomanNumerals();
+
+    const actualRomanNumeral33467 = romanNumerals.toRoman(33467);
+    const actualRomanNumeral444467 = romanNumerals.toRoman(444467);
+    const actualRomanNumeral869849 = romanNumerals.toRoman(869849);
+
+    assert.equal(actualRomanNumeral33467, expectedRomanNumeral33467, messageForNumber33467Conversion);
+    assert.equal(actualRomanNumeral444467, expectedRomanNumeral444467, messageForNumber444467Conversion);
+    assert.equal(actualRomanNumeral869849, expectedRomanNumeral869849, messageForNumber869849Conversion);
 
     assert.end();
 });
